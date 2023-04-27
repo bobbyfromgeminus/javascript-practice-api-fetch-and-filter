66b4bf2d5228058d966859537b7d78fa30ccec00
bobbyfromgeminus <info.geminus@gmail.com>
2023-04-27 15:51:22 +0200

    add jquery ajax mode

--- app.js
+++ app.js
@@ -2,52 +2,43 @@
 
 // DEKLARÁCIÓK _______________________________________________________________________________________________________________________________________________________
 
-    const userTableContainer = document.getElementById('user-list');    // felhasználók táblázatát befoglaló konténer div
+    const userTableBody = document.querySelector('tbody');              // felhasználók táblázatát befoglaló tbody
     const search = document.getElementById('search');                   // kereső input mező
     const showUsersButton = document.querySelector('button');           // adatokat lekérdező gomb
+    const apiURL = 'https://jsonplaceholder.typicode.com/users';        // API url
 
     let usersArray = [];                                                // adatokat tároló tömb változó (kezdő értéke üres tömb)
 
 
 // USEREK LEKÉRÉSE API-TÓL ___________________________________________________________________________________________________________________________________________
 
-    const getUsers = () => {
-        fetch('https://jsonplaceholder.typicode.com/users')             // meghívjuk az API users lábát, lekérve a user adatokat json formátumban
+    const getUsersFetch = () => {
+        fetch(apiURL)                                                   // meghívjuk az API users lábát, lekérve a user adatokat json formátumban
         .then(response => response.json())
         .then(data => {
             usersArray = data;                                          // a kapott json-t átadjuk a 9. sorban létrehozott üres tömbnek (későbbiekben ezzel dolgozunk)
         });
     }
 
+    const getUsersAjax = () => {
+        $.ajax({
+            url: apiURL,
+            method: "GET",
+            dataType: "json",
+            success: function(response) {
+                usersArray = response; 
+            },
+            error: function(xhr, status, error) {
+                console.log(error);
+            }
+        });
+    }
+      
+
 
 // TÁBLÁZAT ELEMEINEK ÖSSZEÁLLÍTÁSA __________________________________________________________________________________________________________________________________
 
-    // 1. Táblázat fejléc készítése (a tábla nyitó elemét, a fejlécét és a törzs nyitó elemét tartalmazza)
-
-        const createTableHeader = () => {
-            const tableHeader = `   <table class="table table-striped">
-                                        <thead>
-                                            <tr>
-                                                <th scope="col">#</th>
-                                                <th scope="col">Name</th>
-                                                <th scope="col">UserName</th>
-                                                <th scope="col">e-mail</th>
-                                            </tr>
-                                        </thead>
-                                    <tbody>`;
-            return tableHeader;
-        }
-
-
-    // 2. Táblázat lábléc készítése (a táblázat törzsének záró és a táblázat záró elemét tartalmazza)
-
-        const createTableFooter = () => {
-            const tableFooter = `</tbody></table>`;
-            return tableFooter;
-        }
-
-
-    // 3. Táblázat sor készítése (egyetlen userből készít egyetlen sort a táblázathoz)
+    // Táblázat sor készítése (egyetlen userből készít egyetlen sort a táblázathoz)
 
         const createTableRow = (user) => {
             const tableRow = ` <tr>
@@ -62,7 +53,7 @@
 
     // 4. Teljes táblázat összeállítása (a három részelemből összeállítja a teljes táblázatot)
 
-        const createFullTable = (users) => {
+        const createTableBody = (users) => {
             let tableBody = ``;                                         // létrehozunk egy üres stringet
 
             users.forEach(user => {                                     // bejárjuk a usereket tartalmazó json tömbünket
@@ -70,9 +61,7 @@
             });
                 
                                                                         // összepakoljuk a részegységekből a teljes táblázatot
-            userTableContainer.innerHTML = createTableHeader()          // a fejlécet
-                                            + tableBody                 // a tábla-törzset
-                                            + createTableFooter();      // a látlécet
+            userTableBody.innerHTML = tableBody;                        // átadjuk a tbodynak
 
         }
 
@@ -86,7 +75,7 @@
                                             item.name.toLowerCase()     // a callback függvényben kisbetűsítem a tömb elem (egy user) name tulajdonságát...
                                             .includes(searchString) );  // majd kigyűjtöm azokat, amelyekben megtalálható a kisbetűsített keresőszó
                                             
-        createFullTable(resultArray);                                   // újrageneráltatom a táblázatot de már nem a teljes user tömbbel, hanem a szűrt résztömbbel
+        createTableBody(resultArray);                                   // újrageneráltatom a táblázatot de már nem a teljes user tömbbel, hanem a szűrt résztömbbel
     }
 
 
@@ -94,7 +83,7 @@
 
     // User-lista generáló gomb megnyomása
     showUsersButton.addEventListener('click', () => {                   // az adatlekérő gomb kattintás eseményét figyeljük
-                                        createFullTable(usersArray)     // a táblageneráló függvényt adjuk át neki, de mivel ez a függvény paramétert vár (ki kell tenni a kerek zárójelet)
+                                        createTableBody(usersArray)     // a táblageneráló függvényt adjuk át neki, de mivel ez a függvény paramétert vár (ki kell tenni a kerek zárójelet)
                                     });                                 // ezért becsomagoltuk egy paraméter nélküli függvénybe, így nem hívódik meg azonnal, csak a kattintásra
 
     // Keresőmezőn történő billantyűfelengedés
@@ -103,4 +92,7 @@
 
 // USEREK TÉNYLEGES LEKÉRÉSE API-TÓL _________________________________________________________________________________________________________________________________
 
-    getUsers();                                                         // meghívjuk az adatlekérő függvényt, ami az API-tól lekéri a usereket és elmenti egy változóba (usersArray)
+
+// meghívjuk az adatlekérő függvényt, ami az API-tól lekéri a usereket és elmenti egy változóba (usersArray)
+getUsersFetch();
+//getUsersAjax();
